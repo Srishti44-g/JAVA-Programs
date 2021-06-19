89018ac6811e0e4d41d889e54d678b09eac99f3a
Srishti Gauraha <67550382+Srishti44-g@users.noreply.github.com>
2021-06-19 07:54:52 +0530

    Create LtoD1.java

---
+++ Beginner's Guide programs/Introducing Data Types and Operators.java/LtoD1.java
@@ -0,0 +1,11 @@
+// ***This program will not compile . ***
+class LtoD1{
+public static void main(Strig args[])
+ {
+  long L;
+  double D;
+  D = 100123285.0;
+  L = D;  //IIlegal!!!
+  System.out.println("L and D:  "+ L +" "+D);
+ }
+}
