c1f8b05243732c7f24dd9e0ddd1e2b485dbae9ed
Srishti Gauraha <67550382+Srishti44-g@users.noreply.github.com>
2021-06-19 07:07:47 +0530

    Create SideEffects.java

---
+++ Beginner's Guide programs/Introducing Data Types and Operators.java/SideEffects.java
@@ -0,0 +1,19 @@
+//Side effects ca be important.
+class  SideEffects{
+   public static void main(String args[])
+  {
+ int  i;
+ i =0;
+/* Here, i is still incremented even though
+    the if statements fails. */
+if(false & (++i < 100))
+   System.out.println("this won't be displayed");
+System.out.println("if statements executed: " + i);  //diplays 1
+/* In this case , i is not incremented because
+     the short-circuit operator skips the increment. */
+if (false && (++i <100) )
+  System.out.println("this won't be displayed");
+System.out.println("if statement executed : "+ i);  //still 1 !!
+
+  }
+}
