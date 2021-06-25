1b60b2344db176b4dbee82abf101f7d8ffaf6e21
Srishti Gauraha <67550382+Srishti44-g@users.noreply.github.com>
2021-06-25 07:14:27 +0530

    Create Comma.java

---
+++ Beginner's Guide programs/Program Control Statements.java/Comma.java
@@ -0,0 +1,8 @@
+public class Comma {
+    public static void main(String args[]){
+        int i,j;
+        for(i =0,j=0 ; i<j ;i++,j--)
+            System.out.println("i and j :"+ i +" "+ j);
+    }
+    
+}
