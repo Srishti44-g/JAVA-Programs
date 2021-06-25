68a7dab232ab54ffe0196da69ff53f386be218e3
Srishti Gauraha <67550382+Srishti44-g@users.noreply.github.com>
2021-06-25 06:58:50 +0530

    Create SqrRoot.java

---
+++ Beginner's Guide programs/Program Control Statements.java/SqrRoot.java
@@ -0,0 +1,15 @@
+// Show square roots of 1 to 99 and the rounding errors.
+public class SqrRoot{
+    public static void main(String args[]){
+    double num , sroot,rerr;
+    for(num =1 ; num<100.0 ;num++)
+    {
+        sroot = Math.sqrt(num);
+        System.out.println("Square root of "+ num +"is"+ sroot);
+        //compute rounding error
+        rerr = num -(sroot*sroot);
+        System.out.println("Rounding error is:"+ rerr);
+    }
+
+    }
+}
