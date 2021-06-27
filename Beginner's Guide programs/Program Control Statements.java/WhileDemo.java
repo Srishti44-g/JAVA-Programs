cc5439d1d0990a5487c9f9147e7c88e9c8e07c47
Srishti Gauraha <67550382+Srishti44-g@users.noreply.github.com>
2021-06-27 15:52:24 +0530

    Create WhileDemo.java

---
+++ Beginner's Guide programs/Program Control Statements.java/WhileDemo.java
@@ -0,0 +1,12 @@
+//Demonstrate time while loop.
+class WhileDemo{
+    public static void main(String args[]){
+        char ch;
+        //print the alphabet using a while loop.
+        ch ='a';
+        while(ch <='z'){
+            Sytem.out.print(ch);
+            ch++;
+        }
+    }
+}
